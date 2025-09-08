d3b854c6ab00afcc08f19d600aeda69338ccf30a
Nei-Dev <neibusdev@gmail.com>
2025-09-07 19:52:44 -0500

    Actualización a modules js

--- index.mjs
+++ index.mjs
@@ -1,28 +1,28 @@
 import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import Handlebars from 'handlebars';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 const sns = new SNSClient({ region: process.env.AWS_REGION });
 const topicArn = process.env.SNS_TOPIC_ARN;
 
 // Pre-load and compile the Handlebars template
-const templatePath = path.join(__dirname, 'template', 'credit.hbs');
+const templatePath = path.join(dirname, 'template', 'credit.hbs');
 const source = fs.readFileSync(templatePath, 'utf8');
 const template = Handlebars.compile(source);
 
 export const handler = async (event) => {
   for (const record of event.Records) {
-    const {
-      name,
-      approved,
-      amount,
-    } = JSON.parse(record.body);
-    const subject = approved ? 'Crédito Aprobado' : 'Crédito Rechazado';
+    const { name, isApproved, amount } = JSON.parse(record.body);
+    const subject = isApproved ? 'Crédito Aprobado' : 'Crédito Rechazado';
 
     const messageBody = template({
       name,
-      approved,
+      isApproved,
       amount
     });
 
